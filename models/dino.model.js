d1e02a5a6365b400b5473fd868e2c51bdaae9d0e
Thomas Kirst <thomas@moneygarden.de>
2021-12-23 14:27:17 +0100

    Added mongoose and removed simple mongodb API calls

---
+++ models/dino.model.js
@@ -0,0 +1,12 @@
+import { Db } from 'mongodb';
+import mongoose from 'mongoose';
+
+const dinoSchema = new mongoose.Schema({
+  name: String,
+  type: String,
+  vegan: Boolean,
+});
+
+const Dino = mongoose.model('Dino', dinoSchema);
+
+export default Dino;
--- server.js
+++ server.js
@@ -1,61 +1,43 @@
 import express from 'express';
-
-// MongoDB Datenbank-Treiber (die Schnittstelle zwischen unserem Server und der DB)
-import { ObjectId } from 'mongodb';
-
-import databaseClient from './lib/db.client.js';
+import mongoose from 'mongoose';
+import Dino from './models/dino.model.js';
 
 // Express-Server erstellen
 const server = express();
 
+const connectionString = 'mongodb://localhost:27017/flaky-dinosaurs';
+mongoose.connect(connectionString);
+
 // Middleware zum JSON parsen (req.body ausstatten)
 server.use(express.json());
 
-server.get('/dinos/:dinoId', async (req, res) => {
-  const dinoId = req.params.dinoId;
-
-  await databaseClient.connect();
-  const db = databaseClient.db('flaky-dinosaurs');
-  const collection = db.collection('dinosaurs');
-
-  const foundDino = await collection.findOne({ _id: ObjectId(dinoId) });
-  res.json(foundDino);
+// Alle Dinos holen
+server.get('/dinos', async (req, res) => {
+  const dinos = await Dino.find();
+  res.json(dinos); //JSON.stringify JSON.parse(Plain JSON text aus dem Datenpaket)
 });
 
-server.get('/dinos', async (req, res) => {
-  await databaseClient.connect();
-  const db = databaseClient.db('flaky-dinosaurs');
-  const collection = db.collection('dinosaurs');
-
-  const dinos = await collection.find().toArray();
-  res.json(dinos); //JSON.stringify JSON.parse(Plain JSON text aus dem Datenpaket)
+server.get('/dinos/:dinoId', async (req, res) => {
+  const dinoId = req.params.dinoId; // 61c341db47e84456ca7d44bb
+  const foundDino = await Dino.findById(dinoId);
+  res.json(foundDino);
 });
 
 // Erstelle einen neuen Dino
 server.post('/dinos', async (req, res) => {
   // Neues Dino-Objekt aus den Request-Daten erstellen
-  const dinosaur = {
+  const dinosaur = new Dino({
     name: req.body.name, // Monica
     type: req.body.type, // null
     vegan: req.body.vegan, // null
-  };
-
-  // Verbindung herstellen (asynchron)
-  await databaseClient.connect();
-  // Datenbank auswählen
-  const db = databaseClient.db('flaky-dinosaurs');
-
-  // Sammlung aller Dokumente (Dinos)
-  const collection = db.collection('dinosaurs');
-
-  // Füge ein neues Dokument in die Sammlung ein und warte auf das Ergebnis
-  const result = await collection.insertOne(dinosaur);
-
-  // Ergebnis auswerten und die erzeugte Id an den databaseClient / Nutzer zurücksenden
-  res.json({
-    message:
-      'You successfully inserted a new dino with the id: ' + result.insertedId,
   });
+
+  try {
+    const result = await dinosaur.save();
+    res.json(result);
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 server.listen(4000, () => {

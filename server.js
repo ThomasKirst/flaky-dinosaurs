6bb41196aba001bfa8e6c6e132b108558e8c6f9b
Thomas Kirst <thomas@moneygarden.de>
2021-12-23 14:49:57 +0100

    Added basic update and delete functions for dinos

--- server.js
+++ server.js
@@ -40,6 +40,30 @@ server.post('/dinos', async (req, res) => {
   }
 });
 
+server.put('/dinos/:dinoId', async (req, res) => {
+  const dinoId = req.params.dinoId;
+  const dino = req.body;
+
+  const updatedDino = await Dino.findByIdAndUpdate(dinoId, dino, {
+    returnDocument: 'after',
+  });
+  res.json(updatedDino);
+});
+
+server.delete('/dinos/:dinoId', async (req, res) => {
+  const dinoId = req.params.dinoId;
+  try {
+    const result = await Dino.findByIdAndDelete(dinoId);
+    if (result) {
+      res.json(result);
+    } else {
+      res.status(404).json({ message: 'This dino could not be found!' });
+    }
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 server.listen(4000, () => {
   console.log('Dino-Server is up and running');
 });
